1e282d7a802d48bd7241f74c9a1bd131f0fd88c6
LAPTOP-RTVOEFJC\vikra <triveni.wadkar@gmail.com>
2018-07-04 11:57:09 +0200

    Add or improve comments in the spec file

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,32 +1,29 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
+/* Placing all of tests within the $() function,
+ * since some of these tests may require DOM elements,
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-	/* This is our first test suite - a test suite just contains
+	/* This is the first test suite - a test suite just contains
 	 * a related set of tests. This suite is all about the RSS
 	 * feeds definitions, the allFeeds variable in our application.
 	 */
 	describe('RSS Feeds', () => {
-		/* This is our first test - it tests to make sure that the
+		/* It tests to make sure that the
 		 * allFeeds variable has been defined and that it is not
-		 * empty. Experiment with this before you get started on
-		 * the rest of this project. What happens when you change
-		 * allFeeds in app.js to be an empty array and refresh the
-		 * page?
+		 * empty. 
 		 */
 		it('are defined', () => {
 			expect(allFeeds).toBeDefined();
 			expect(allFeeds.length).not.toBe(0);
 		});
 
-		/* TODO: Write a test that loops through each feed
+		/*  This test that loops through each feed
 		 * in the allFeeds object and ensures it has a URL defined
 		 * and that the URL is not empty.
 		 */
@@ -37,7 +34,7 @@ $(function() {
 				});
 			});
 
-		/* TODO: Write a test that loops through each feed
+		/* This is a test that loops through each feed
 		 * in the allFeeds object and ensures it has a name defined
 		 * and that the name is not empty.
 		 */
@@ -51,12 +48,12 @@ $(function() {
 	});
 
 
-	/* TODO: Write a new test suite named "The menu" */
+	/*  A new test suite named "The menu" */
 	describe('The menu', () => {
 		var body = document.body;
 		var menuIcon = document.querySelector('.menu-icon-link');
 		
-		/* TODO: Write a test that ensures the menu element is
+		/* Test that ensures the menu element is
 		 * hidden by default. You'll have to analyze the HTML and
 		 * the CSS to determine how we're performing the
 		 * hiding/showing of the menu element.
@@ -65,7 +62,7 @@ $(function() {
 			expect(body.className).toContain('menu-hidden');
 		});
 		
-		/* TODO: Write a test that ensures the menu changes
+		/* The test that ensures the menu changes
 		 * visibility when the menu icon is clicked. This test
 		 * should have two expectations: does the menu display when
 		 * clicked and does it hide when clicked again.
@@ -81,12 +78,12 @@ $(function() {
 	});
 
 
-	/* TODO: Write a new test suite named "Initial Entries" */
+	/* A new test suite named "Initial Entries" */
 	describe('Initial Entries', () => {
-		/* TODO: Write a test that ensures when the loadFeed
+		/* A test that ensures when the loadFeed
 		 * function is called and completes its work, there is at least
 		 * a single .entry element within the .feed container.
-		 * Remember, loadFeed() is asynchronous so this test will require
+		 * loadFeed() is asynchronous so this test includes
 		 * the use of Jasmine's beforeEach and asynchronous done() function.
 		 */
 		beforeEach((done) => {
@@ -101,14 +98,13 @@ $(function() {
 					expect(feedEntries.length).toBeGreaterThan(0);
 			done();
 		});		
-
+		
 	});
 
-	/* TODO: Write a new test suite named "New Feed Selection" */
+	/* A new test suite named "New Feed Selection" */
 	describe('New Feed Selection', () => {
-		/* TODO: Write a test that ensures when a new feed is loaded
+		/* Test that ensures when a new feed is loaded
 		 * by the loadFeed function that the content actually changes.
-		 * Remember, loadFeed() is asynchronous.
 		 */
 		var initialFeedSelection;
 		beforeEach((done) => {
